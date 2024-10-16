feb2f8b788765cdda8925a62dc15c505450da85e
João Miguel <joaomiguelmorais1108@gmail.com>
2024-10-16 20:18:17 -0300

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,6 @@
+numero1 = float(input("Digite um numero maior que zero"))
+numero2 = float(input("Digite outro numero maior que zero"))
+
+calculo = numero1**numero2
+
+print(f"{numero1} elavado á {numero2} é {calculo}")
