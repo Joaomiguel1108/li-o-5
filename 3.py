583a1cc9b72ec330ac506c33df7974657e3096cd
João Miguel <joaomiguelmorais1108@gmail.com>
2024-10-16 20:21:25 -0300

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,7 @@
+pes = float(input("Digite a media em pés"))
+
+polegadas = pes * 12
+jardas = pes / 3
+milhas = jardas / 1760
+
+print(f"convertendo {pes} pés para polegadas, o resultado é {polegadas}, convertendo para jardas é {jardas}, e de jardas para milhas é {milhas}")
