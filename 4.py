86b59553f86cff3ffa47dc7e47e85438d40f8b02
João Miguel <joaomiguelmorais1108@gmail.com>
2024-10-16 20:30:59 -0300

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,7 @@
+ano = int(input("Digite seu ano de nascimento"))
+ano_atual = int(input("Digite o ano atual"))
+
+idade = ano_atual - ano
+idade_2050 = 2050 - ano
+
+print(f"Como você tem {idade}, em 2050 você terá {idade_2050} anos,")
